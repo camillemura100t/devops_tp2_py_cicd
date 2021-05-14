b777f558ab0756a14baabd8bd2aa42c33974fe32
camillemura100t <82384233+camillemura100t@users.noreply.github.com>
2021-05-14 12:35:10 +0200

    Update ApplicationUtils.py

--- app/ApplicationUtils.py
+++ app/ApplicationUtils.py
@@ -1,8 +1,17 @@
 import requests
 
 def listApplications():
-    curl -u admin:S0n@N3x951/ -X GET 'http://192.168.0.101:8086/service/rest/v1/components?repository=apprepo'
 
+    url = "http://192.168.0.101:8086/service/rest/v1/components?repository=apprepo"
+
+    payload={}
+    files=[]
+    headers = {'Authorization': 'Basic YWRtaW46UzBuQE4zeDk1MS8='}
+
+    response = requests.request("GET", url, headers=headers, data=payload, files=files)
+
+    print(response.text)
+    
 def AddApplication(directory,filepath,filenametodisplay):
     
     url = "http://192.168.0.101:8086/service/rest/v1/components?repository=apprepo"
