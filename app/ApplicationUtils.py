03496c6c6b1c76fc12ec66a01ea08089380774c6
camillemura100t <82384233+camillemura100t@users.noreply.github.com>
2021-05-14 15:19:58 +0200

    Update ApplicationUtils.py

--- app/ApplicationUtils.py
+++ app/ApplicationUtils.py
@@ -11,15 +11,14 @@ def listApplications():
     response = requests.request("GET", url, headers=headers, data=payload, files=files)
 
     print(response.text)
-    
-def AddApplication(directory,filepath,filenametodisplay):
-    
+
+def addApplication(directory,filepath,filenametodisplay):
+
     url = "http://192.168.0.101:8086/service/rest/v1/components?repository=apprepo"
 
     payload={'raw.directory': directory,
-    'raw.asset1': filepath,
     'raw.asset1.filename': filenametodisplay}
-    files=[]
+    files= {'raw.asset1' : (open(filepath,'rb'))}
     headers = {'Authorization': 'Basic YWRtaW46UzBuQE4zeDk1MS8='}
 
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
